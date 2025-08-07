0c01d174a66acd7e0e91c275aa5ca599f92b3b62
mhndra <mahendranabiha11@gmail.com>
2025-08-07 19:30:58 +0700

    Add files via upload

---
+++ dags/postgres_etl_pipeline.py
@@ -0,0 +1,107 @@
+from datetime import datetime, timedelta
+
+import pandas as pd
+import requests
+from airflow import DAG
+from airflow.decorators import task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from sqlalchemy import create_engine
+
+with DAG(
+    dag_id="postgres_etl_pipeline",
+    start_date=datetime(2025, 1, 1),
+    schedule="@daily",
+    catchup=True,
+    max_active_runs=1,
+    default_args={"retries": 3, "retry_delay": timedelta(minutes=5)},
+) as dag:
+    # Create a task using the TaskFlow API
+    @task(depends_on_past=True)
+    def create_table():
+        # Connect to the Postgres database
+        hook = PostgresHook(postgres_conn_id="market_postgres_database_conn")
+        #  Query to create the market_data table if it does not exist
+        create_table_query = """ CREATE TABLE IF NOT EXISTS market_data(
+            status VARCHAR(255),
+            date DATE,
+            symbol VARCHAR(10),
+            open NUMERIC(10, 2),
+            high NUMERIC(10, 2),
+            low NUMERIC(10, 2),
+            close NUMERIC(10, 2),
+            volume INT,
+            "afterHours" NUMERIC(10, 2),
+            "preMarket" NUMERIC(10, 2)
+            ); """
+        # Execute the query to create the table
+        hook.run(create_table_query)
+
+    @task()
+    def hit_polygon_api(**context):
+        # Instantiate a list of tickers that will be pulled and looped over
+        stock_ticker = "AMZN"
+        # Set variables
+        polygon_api_key = "_nSOgY2sw_p9LEGXVZVleuvYdxohITF3"
+        ds = context.get("ds")
+        # Create the URL
+        url = f"https://api.polygon.io/v1/open-close/{stock_ticker}/{ds}?adjusted=true&apiKey={polygon_api_key}"
+        response = requests.get(url)
+        # Return the raw data
+        return response.json()
+
+    @task()
+    def flatten_market_data(polygon_response, **context):
+        # Create a list of headers and a list to store the normalized data in
+        columns = {
+            "status": "closed",
+            "from": context.get("ds"),
+            "symbol": "AMZN",
+            "open": None,
+            "high": None,
+            "low": None,
+            "close": None,
+            "volume": None,
+            "afterHours": None,
+            "preMarket": None,
+        }
+        # Create a list to append the data to
+        flattened_record = []
+        for header_name, default_value in columns.items():
+            # Append the data
+            flattened_record.append(polygon_response.get(header_name, default_value))
+        # Convert to a pandas DataFrame
+        flattened_dataframe = pd.DataFrame(
+            [flattened_record], columns=columns.keys()
+        ).rename(columns={"from": "date"})
+        # Return the DataFrame as a list of dictionaries
+        return flattened_dataframe.to_dict(orient="records")
+
+    @task()
+    def load_market_data(data_records):
+        # Pull the connection
+        market_database_hook = PostgresHook(
+            postgres_conn_id="market_postgres_database_conn"
+        )
+        market_database_conn = market_database_hook.get_connection(
+            market_database_hook.postgres_conn_id
+        )
+        market_database_engine = create_engine(
+            f"postgresql+psycopg2://{market_database_conn.login}:{market_database_conn.password}@{market_database_conn.host}:{market_database_conn.port}/{market_database_conn.schema}"
+        )
+        # Load the table to SQLite, append if it exists
+        flattened_dataframe = pd.DataFrame(data_records)
+        if data_records[0]["status"] == "OK":
+            flattened_dataframe.to_sql(
+                name="market_data",
+                con=market_database_engine,
+                if_exists="append",
+                index=False,
+            )
+        else:
+            pass  # Handle the case where the API response is not OK
+        print(market_database_hook.get_records("SELECT * FROM market_data;"))
+
+    # Set dependencies
+    raw_market_data = hit_polygon_api()
+    transformed_market_data = flatten_market_data(raw_market_data)
+    create_table() >> load_market_data(transformed_market_data)
